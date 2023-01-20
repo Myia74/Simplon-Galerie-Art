464753b6e6edbafbf24bac47a302d811e6f2be59
Myia74 <myiayun74@gmail.com>
2023-01-20 15:56:20 +0100

    deplacement du player dans le decor

--- Assets/Scripts/FpsPlayerController.cs
+++ Assets/Scripts/FpsPlayerController.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class FpsPlayerController : MonoBehaviour
 {
-    
+    private float positionX;
+    private float positionZ;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speedPlayer = 14.0f;
+
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        positionX = Input.GetAxis("Horizontal");
+        positionZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * positionX + transform.forward * positionZ;
+
+        controller.Move(move * speedPlayer * Time.deltaTime);
+
     }
 }
--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -14,7 +14,7 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;  // pour bloquer la rotation de la camera via la souris 
     }
 
     // Update is called once per frame
@@ -24,12 +24,24 @@ public class MouseLook : MonoBehaviour
         mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime; // instanciation souris camera sur Y
 
         xRotation -= mouseY;  //rotation avec la souris sur axe Y
+        xRotation = Mathf.Clamp(xRotation, -90, 90);  
+        // va fonctionner avec cursor lock
+        /*Mathf.Clamp est une fonction qui permet de limiter la valeur d'une variable à une plage de valeurs spécifiée normalement 3
+         * Il prend trois paramètres : la valeur à limiter, la valeur minimale de la plage et la valeur maximale de la plage. 
+         * Si la valeur est inférieure à la valeur minimale, la fonction renvoie la valeur minimale. 
+         * Si la valeur est supérieure à la valeur maximale, la fonction renvoie la valeur maximale. 
+         * Sinon, elle renvoie la valeur telle quelle         * 
+         */
 
-        //application de la rotation sur Y  (Quaternion.Euler est une méthode qui permet de convertir un vecteur de rotation en degrés en un quaternion de rotation)
+        //application de la rotation sur Y
+        /*(Quaternion.Euler est une méthode qui permet de convertir un vecteur de rotation en degrés en un quaternion de rotation)
+        * Il prend un vecteur de 3 composantes (en x, y et z) qui représente les angles de rotation en degrés sur chacun des axes.
+        */
+
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
 
 
-        fpsPlayerBody.Rotate(Vector3.up * mouseX);  // rotation avec la souris 
+fpsPlayerBody.Rotate(Vector3.up * mouseX);  // rotation avec la souris 
 
-    }
+}
 }
